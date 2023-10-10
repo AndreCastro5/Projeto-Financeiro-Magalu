af05e8af7e83fe9e63a8010c045370e7108a0442
Your Name <andre.castro2356@gmail.com>
2023-10-10 19:08:58 -0300

    first commit

---
+++ Projeto financeiro magalu.py
@@ -0,0 +1,89 @@
+# Libs para Modelagem e Matrizes
+import numpy as np
+import pandas as pd 
+
+# Libs para análises gráficas 
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import plotly.graph_objects as go 
+
+# Lib para ignorar avisos
+import warnings 
+
+# Desabilitando avisos 
+warnings.filterwarnings('ignore')
+
+# Lib para ler o arquivo Df
+import os 
+
+# Lendo dados 
+os.chdir('C:\\Users\\andre\\OneDrive\\Área de Trabalho\\python\\Projeto Financeiro Magalu')
+Base_dados = pd.read_excel('Vase_004 - Magalu - Sem Resolução.xlsx')
+
+# Verificar dados 
+print(Base_dados.shape)
+
+print(Base_dados.head())
+
+print(Base_dados.info)
+
+# Análises por tabela 
+print(Base_dados.describe())
+
+# Series Temporais 
+Dados = Base_dados.set_index('Data')
+
+print(Dados.head)
+
+# Grafico Análise das Ações da magalu - Fechamento 
+
+plt.style.use ('seaborn-darkgrid')
+plt.figure( figsize=(16,5))
+plt.title('Análise das Ações da magalu - Fechamento ', fontsize= 16, loc='left')
+plt.plot(Dados.index, Dados['Fechamento'])
+
+plt.xlabel('Periodo da Coração')
+plt.ylabel('Valor da Ação')
+plt.show();
+
+print(Dados.tail())
+
+# Análise de media movel e media tendencia 
+Media_Movel = Dados['Fechamento'].rolling(5).mean()
+Media_Tendencia = Dados['Fechamento'].rolling(30).mean()
+
+plt.style.use('seaborn-darkgrid')
+plt.figure( figsize=(16, 5) )
+plt.title('Análise das ações da magalu - Fechamento', fontsize=15, loc='left')
+
+plt.plot( Dados.index, Dados['Fechamento'] )
+plt.plot( Dados.index, Media_Movel )
+plt.plot( Dados.index, Media_Tendencia )
+
+plt.xlabel('Período da Coração')
+plt.ylabel('Valor da Ação (R$)')
+plt.show();
+
+#Boxplot Mensal 
+Base_dados['Mês'] = Base_dados['Data'].dt.month 
+
+plt.figure ( figsize= (16,5))
+sns.boxplot(data=Base_dados, x='Mês', y='Fechamento')
+plt.show();
+
+# Gráfico de comparação 
+Grafico = go.Figure(
+    data=[
+        go.Candlestick(
+            x= Dados.index,
+            open= Dados['Abertura'],
+            high = Dados['Maior'],
+            low = Dados['Menor'],
+            close = Dados['Fechamento'],
+        )
+    ]
+)
+
+Grafico.update_layout( xaxis_rangeslider_visible=False)
+
+Grafico.show()
